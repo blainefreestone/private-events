b73511aecfd0182639ed6de954487c1947f23500
blainefreestone <blainefreestone01@gmail.com>
2023-11-17 21:52:10 -0800

    User model created with devise.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_18_052852) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_18_055013) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.datetime "date_and_time"
@@ -18,6 +18,19 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_18_052852) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
