43b5ed2f03391de5f8db3c0ba2fcfa2416a10d47
blainefreestone <blainefreestone01@gmail.com>
2023-11-24 00:17:45 -0800

    Added scopes to query future and past events for the index view.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,15 @@ class Event < ApplicationRecord
     validates :title, :date_and_time, :location, :description, presence: true
     has_many :event_attendings, foreign_key: "attended_event"
     has_many :attendees, through: :event_attendings, source: :attendee
+
+    scope :future, -> { where("DATE(date_and_time) > ?", DateTime.now) }
+    scope :past, -> { where("DATE(date_and_time) < ?", DateTime.now) }
+
+    # def self.future
+    #     Event.all.where("DATE(date_and_time) > ?", DateTime.now)
+    # end
+
+    # def self.past
+    #     Event.all.where("DATE(date_and_time) < ?", DateTime.now)
+    # end
 end
